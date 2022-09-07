9bbf3cc04ade83dbcdfd0a004dc6b2a09135e908
GiorgiKalatozishvili <giorgi.kalatozishvili.1@btu.edu.ge>
2022-09-07 20:37:37 +0400

    header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header absolute-center cur-po">
+      <span className="heading-gradient">Develops</span>
+    </div>
+  );
 };
 
 export default Header;
--- src/components/TopFold/index.js
+++ src/components/TopFold/index.js
@@ -2,7 +2,20 @@ import React from "react";
 import "./topFold.css";
 
 const TopFold = () => {
-  return <div>TopFold</div>;
+  return (
+    <div className="topfold absolute-center">
+      <div className="tf-left">
+        <div className="tf-heading">
+          Discover collect, & sell <span>Extraordinary</span> NFTs
+        </div>
+        <div className="tf-description">
+          the leading NFT Marketplace on EthereumHome to the next generation of
+          digital creators. Discover the best NFT collections.
+        </div>
+      </div>
+      <div className="tf-right"></div>
+    </div>
+  );
 };
 
 export default TopFold;
